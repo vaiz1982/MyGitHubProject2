9252dd0d06257b99b156296477f97caa772548ca
alexeyvoytsekhovich <vaiz@inbox.ru>
2023-02-09 01:05:43 -0500

    Initial commit

---
+++ src/day2GitHub/firstGitHub.java
@@ -0,0 +1,12 @@
+package day2GitHub;
+
+public class firstGitHub {
+    public static void main(String[] args) {
+        System.out.println("Hello B7");
+        //its new project we have to make integration first again -go up to VCS
+           // (enable version) -first we have to do after create project!
+
+
+
+    }
+}
