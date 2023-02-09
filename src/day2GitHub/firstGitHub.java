34cc80235e79d8e2a559244b6bbacece00e070d8
alexeyvoytsekhovich <vaiz@inbox.ru>
2023-02-09 01:24:20 -0500

    my second class added

--- src/day2GitHub/firstGitHub.java
+++ src/day2GitHub/firstGitHub.java
@@ -5,6 +5,14 @@ public class firstGitHub {
         System.out.println("Hello B7");
         //its new project we have to make integration first again -go up to VCS
            // (enable version) -first we have to do after create project!
+        /*
+        * red working staging
+        * ADD green cash/index/staging area - ITS NOT ON GIT UP EYT ITS IN THE MIDDLE CASH
+        * COMMIT white LOCAL REPO MASTER LOCAL BRANCH ALL ON PC after here is delete from cash!
+        //
+         */
+
+        // even before commit we have to share on my gitHub account! to safe it just in case!
 
 
 
